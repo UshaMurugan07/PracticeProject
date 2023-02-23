154260b44a546aec6f552059d57a9c8037010f6c
UshaMurugan07 <ushamurugan66@gmail.com>
2023-02-23 21:29:24 +0530

    added 3 more method in usha branch

--- src/test/java/org/tcs/Class21.java
+++ src/test/java/org/tcs/Class21.java
@@ -10,4 +10,13 @@ System.out.println("2");
 	private void method3() {
 System.out.println("3");
 }
+private void method4() {
+System.out.println("usha");
+}
+private void method5() {
+System.out.println("dhana");
+}
+private void method6() {
+System.out.println("revathi");
+}
 }
